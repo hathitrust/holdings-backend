f7f6974f42bc06117cca420b4525c8e5aa7b06c0
Martin Warin <mwarin@umich.edu>
2020-02-13 14:53:19 -0500

    transliterating to random 0..9 instead of random a..z

--- get_delta/make_sample_files.sh
+++ get_delta/make_sample_files.sh
@@ -1,10 +1,10 @@
-# Get some random-transliterated test data from 2 .gz files, for
-# purposes of doing quick tests of delta generation.
+# Get some random-transliterated test data from 2 .gz files,
+# for purposes of doing quick tests of delta generation.
 # Usage:
 # $ bash make_sample_files.sh <old_file> <new_file>
 
-# Get 10 random letters from the alphabet
-rand_az=`perl -e "print join('', (sort {rand() <=> 0.5} ('a'..'z'))[0..9])"`;
+# Turn 0..9 into random range, for use in tr later.
+rand_int_rng=`perl -e "print join('', (sort {rand() <=> 0.5} (0..9)))"`;
 
 # Put stuff here.
 data_dir='../data';
@@ -15,7 +15,12 @@ new_fn=$2;
 function get_sample_translit {
     fn=$1;
     old_new=$2;
-    zcat $fn | head -100000 | tr '[0-9]' "[$rand_az]" | awk -F'\t' '$1 != $2' | sort > $data_dir/${old_new}_head_sample_obfusc.txt    
+
+    zcat -f $fn |
+	head -100000 |
+	awk -F'\t' '$1 != $2' |
+	tr '[0-9]' "[$rand_int_rng]" |
+	sort > $data_dir/${old_new}_head_sample_obfusc.txt    
 }
 
 # Transliterate old and new with (the same) random letters.
