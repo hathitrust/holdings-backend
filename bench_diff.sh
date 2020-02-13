d6d6e595720753b06c288052a0e0045e32b71e4f
Martin Warin <mwarin@umich.edu>
2020-02-13 14:26:54 -0500

    old and useless

--- bench_diff.sh
+++
@@ -1,6 +0,0 @@
-# Checking if there are any speed gains in using threads.
-for i in {1..10}
-do
-    time perl oclc_concordance/setDiff.pl data/old_3M.txt data/new_3M.txt          > /dev/null;
-    time perl oclc_concordance/setDiff.pl data/old_3M.txt data/new_3M.txt --thread > /dev/null;
-done
