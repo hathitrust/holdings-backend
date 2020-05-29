1667a472973fda13c123cc8d954ea705accdb8a5
Bill Dueber <bill@dueber.com>
2020-05-29 12:02:32 -0400

    HT-2420 Fix add_ocn_resolutions
      * Fix error from missing `count` initialization
      * Allow STDIN, gzipped, or plain files for input viz zinzout
      * Create Waypoint class and ppnum (pretty-print number) function
        for easier and prettier logging
      * Add real logging to add_ocn_resolutions.rb

--- bin/add_ocn_resolutions.rb
+++ bin/add_ocn_resolutions.rb
@@ -5,17 +5,31 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require "bundler/setup"
 require "cluster_ocn_resolution"
 require "ocn_resolution"
+require "zinzout"
+require "logger"
+require "utils/waypoint"
+require "utils/ppnum"
 
 Mongoid.load!("mongoid.yml", :test)
 
-File.open(ARGV.shift).each do |line|
+BATCH_SIZE = 10_000
+logger = Logger.new(STDOUT)
+waypoint = Utils::Waypoint.new
+
+# rubocop:disable Layout/LineLength
+logger.info "Starting #{Pathname.new(__FILE__).basename}. Batches of #{ppnum BATCH_SIZE}"
+# rubocop:enable Layout/LineLength
+
+Zinzout.zin(ARGV.shift).each_with_index do |line, count|
   (deprecated, resolved) = line.split.map(&:to_i)
   r = OCNResolution.new(deprecated: deprecated, resolved: resolved)
   c = ClusterOCNResolution.new(r).cluster
   c.save
-  count += 1
 
-  if (count % 10_000).zero?
-    puts "#{Time.now}: #{count} records loaded"
+  if (count % BATCH_SIZE).zero? && !count.zero?
+    waypoint.mark(count)
+    # rubocop:disable Layout/LineLength
+    logger.info "#{ppnum(count, 10)}. This batch #{ppnum(waypoint.batch_records, 5)} in #{ppnum(waypoint.batch_seconds, 4, 1)}s (#{waypoint.batch_rate_str} r/s). Overall #{waypoint.total_rate_str} r/s."
+    # rubocop:enable Layout/LineLength
   end
 end
