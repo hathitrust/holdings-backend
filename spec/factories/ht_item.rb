a5aa96b851e9cfe42b4c47f8b8b6ed0bf8ae4fca
Josh Steverman <jstever@umich.edu>
2020-08-14 17:21:43 -0400

    Add access field to ht_items

--- spec/factories/ht_item.rb
+++ spec/factories/ht_item.rb
@@ -6,6 +6,7 @@ FactoryBot.define do
     item_id { rand(1_000_000).to_s }
     ht_bib_key { rand(1_000_000) }
     rights { ["pd", "ic", "icus", "cc", "pdus"].sample }
+    access { ["allow", "deny"].sample }
     bib_fmt { ["m", "s", "r"].sample }
     enum_chron { ["", "V.1"].sample }
   end
