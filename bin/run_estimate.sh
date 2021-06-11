80b716aae266934477e31c5ac20315766de03ced
Aaron Elkiss <aelkiss@umich.edu>
2021-06-11 15:08:09 -0400

    Fix run estimate wrapper

--- bin/run_estimate.sh
+++ bin/run_estimate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+TIME=$(date +"%Y%m%d%H%M%S")
 if [[ -z $1 ]]; then
-  echo "Usage: $0 /path/to/ocns" 
+  echo "Usage: $0 /path/to/ocns"
   exit 1;
 fi
 
-echo /usr/src/app/bin/run_generic_job.sh bundle exec ruby /usr/src/app/bin/compile_estimated_IC_costs.rb $1
+/usr/src/app/bin/run_generic_job.sh estimate-$TIME bundle exec ruby /usr/src/app/bin/compile_estimated_IC_costs.rb $1
