e8863a48f2f707e516ae7589a4b561fb0aed08e2
mw <mwarin@umich.edu>
2021-07-13 10:27:23 -0500

    script to get all holdings for 1+ member(s)

---
+++ lib/basic_query_report.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+require "services"
+require "cluster"
+
+Services.mongo!
+
+# Pass a mongodb query to the appropriate method
+# and the results will be yielded to you
+class BasicQueryReport
+  # Usage:
+  # query = { ... }
+  # BasicQueryReport.new.aggregate(query) { |result| ... }
+  def aggregate(query, &block)
+    Cluster.collection.aggregate(query).each(&block)
+  end
+end
