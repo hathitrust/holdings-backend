e45ae47afb9be862acf68d6459eb1dfe1b341018
mw <mwarin@umich.edu>
2021-12-17 11:19:33 -0600

    bit of usage

--- bin/reports/compile_eligible_commitments_report.rb
+++ bin/reports/compile_eligible_commitments_report.rb
@@ -2,6 +2,13 @@
 
 require "reports/eligible_commitments"
 
+# Cmdline input: a list of ocns,
+# STDOUT output: a report of which of the input ocns are eligible for commitments.
+# Invoke thusly:
+# $ bundle exec ruby compile_eligible_commitments_report.rb <list_of_ocns>
+# e.g.
+# $ bundle exec ruby compile_eligible_commitments_report.rb 1 2 3
+
 if $PROGRAM_NAME == __FILE__
   report = Reports::EligibleCommitments.new
   puts report.header.join("\t")
