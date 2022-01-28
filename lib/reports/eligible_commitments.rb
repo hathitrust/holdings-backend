e0b0f7fb7f1e3eba0f23ae60897a0104dd7ca611
mw <mwarin@umich.edu>
2022-01-28 15:22:53 -0600

    simplified

--- lib/reports/eligible_commitments.rb
+++ lib/reports/eligible_commitments.rb
@@ -9,17 +9,9 @@ module Reports
   # Invoke via bin/reports/compile_eligible_commitments_report.rb
   class EligibleCommitments
     def header
-      [
-        "organization",
-        "oclc_sym",
-        "ocn",
-        "local_id"
-      ]
+      ["organization", "oclc_sym", "ocn", "local_id"]
     end
 
-    # In order to get the commitments that could have holdings but don't,
-    # get the holdings, the matched pairs, and remove from holdings the ones
-    # that are in a matched pair
     def for_ocns(ocns = [])
       if ocns.empty?
         raise "No ocns given"
@@ -27,21 +19,15 @@ module Reports
 
       ocns.sort.uniq.each do |ocn|
         overlap = Overlap::HoldingCommitment.new(ocn)
-        holdings = overlap.holdings
-        # Matched pairs in a HoldingCommitment is [[com1, hol1], [com2, hol2], ...].
-        overlap.matched_pairs.each do |pair|
-          # Remove the matched hol from holdings ...
-          holdings.delete(pair.last)
-          puts "deleting a holding"
-        end
-        # ... and what you are left with are holdings without commitments.
-        holdings.each do |h|
-          yield [
-            h.organization,
-            organization_oclc_symbol(h.organization),
-            h.ocn,
-            h.local_id
-          ]
+        if overlap.commitments.any?
+          overlap.holdings.each do |h|
+            yield [
+              h.organization,
+              organization_oclc_symbol(h.organization),
+              h.ocn,
+              h.local_id
+            ]
+          end
         end
       end
     end
