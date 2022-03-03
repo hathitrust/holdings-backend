932adf3939410f3e459d295d3057fe5ba8ef3825
mw <mwarin@umich.edu>
2022-03-03 09:08:51 -0600

    Code for replacing commitments.

---
+++ lib/shared_print/replace_record.rb
@@ -0,0 +1,80 @@
+module SharedPrint
+  class ReplaceRecord
+    attr_reader :existing, :replacement
+    def initialize(existing: nil, replacement: nil)
+      @existing = existing
+      @replacement = replacement
+      validate
+    end
+
+    def validate
+      raise ArgumentError, "Missing existing commitment" if @existing.nil?
+      raise ArgumentError, "Missing replacement commitment" if @replacement.nil?
+
+      # The replacement does not have to be a commitment that exists in the db.
+      # It may need to autovivify upon replacing an old one, when reading from file.
+      # Thus we may need to turn a Hash read from a file into a Clusterable::Commitment.
+      if @replacement.instance_of?(Hash)
+        @replacement = Clusterable::Commitment.new(@replacement)
+        Clustering::ClusterCommitment.new(@replacement).cluster.tap(&:save)
+      end
+
+      unless @existing.instance_of?(Clusterable::Commitment)
+        raise ArgumentError, "Existing commitment must be a Clusterable::Commitment"
+      end
+
+      unless @replacement.instance_of?(Clusterable::Commitment)
+        raise ArgumentError, "Replacement must be a Clusterable::Commitment"
+      end
+
+      unless matches.size == 1
+        raise IndexError, "Existing matches must be 1 (is #{matches.size})"
+      end
+    end
+
+    # Replace :existing with :replacement.
+    def apply
+      # Sadly (?) this makes it so that all replaced commitments are deprecated with
+      # the same status "E". Even if previously deprecated with another status.
+      # Status "R" for Replaced was rejected because it would be meaningless.
+      matches.first.deprecate(status: "E", replacement: @replacement)
+      matches.first.save
+    end
+
+    # Original implementation that creates duplicate records with the same _ids in Mongo.
+    def apply_broken
+      @existing.deprecate(status: "E", replacement: @replacement)
+      @existing.save
+    end
+
+    # In order to replace a commitment, we need to know how many matches there are for it.
+    # Ideally, there should be only one.
+    def matches
+      @matches ||= SharedPrint::Finder.new(
+        ocn: [@existing.ocn],
+        organization: [@existing.organization],
+        local_id: [@existing.local_id],
+        deprecated: nil
+      ).commitments.to_a
+    end
+
+    # After apply, replaced should return the freshly deprecated record.
+    def replaced
+      SharedPrint::Finder.new(
+        ocn: [@existing.ocn],
+        organization: [@existing.organization],
+        local_id: [@existing.local_id],
+        deprecated: true
+      ).commitments.to_a.first
+    end
+
+    # Verify that the commitment supposed to get replaced did indeed get replaced.
+    def verify
+      replaced.deprecation_replaced_by.to_s == @replacement._id.to_s
+    end
+
+    def to_s
+      "Replace commitment:\n#{@existing.inspect}\n...with:\n#{@replacement.inspect}"
+    end
+  end
+end
---
+++ spec/shared_print/replace_record_spec.rb
@@ -0,0 +1,159 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "shared_print/finder"
+require "shared_print/replace_record"
+
+RSpec.describe SharedPrint::ReplaceRecord do
+  let(:ocn1) { 1 }
+  let(:ocn9) { 9 }
+  let(:org1) { "umich" }
+  let(:loc1) { "i111" }
+
+  let(:org2) { "yale" }
+  let(:loc2) { "i222" }
+  let(:obj) { Object.new }
+
+  # Spc 1 & 2 have the same ocn, that's the only similarity
+  let(:spc1) { build(:commitment, ocn: ocn1, organization: org1, local_id: loc1) }
+  let(:spc2) { build(:commitment, ocn: ocn1, organization: org2, local_id: loc2) }
+  let(:spc2_hash) { {ocn: ocn1.to_s, organization: org2, local_id: loc2} }
+
+  def get_deprecated
+    SharedPrint::Finder.new(deprecated: true).commitments.to_a
+  end
+
+  def get_active
+    SharedPrint::Finder.new.commitments.to_a
+  end
+
+  # Hooks
+  before(:each) do
+    Cluster.collection.find.delete_many
+  end
+
+  describe "#new(existing: x, replacement: y)" do
+    it "requires 2 args (:existing and :replacement)" do
+      expect { described_class.new }.to raise_error ArgumentError
+    end
+    it "allows :existing to be a Clusterable::Commitment" do
+      cluster_tap_save [spc1, spc2]
+      expect { described_class.new(existing: spc1, replacement: spc2) }.not_to raise_error
+      expect { described_class.new(existing: obj, replacement: spc2) }.to raise_error ArgumentError
+    end
+    it "allows :replacement to be either a hash or a Clusterable::Commitment" do
+      cluster_tap_save [spc1, spc2]
+      expect { described_class.new(existing: spc1, replacement: spc2_hash) }.not_to raise_error
+      expect { described_class.new(existing: spc1, replacement: spc2) }.not_to raise_error
+      expect { described_class.new(existing: spc1, replacement: obj) }.to raise_error ArgumentError
+    end
+  end
+
+  describe "fail gracefully" do
+    it "when the :existing commitment cannot be found" do
+      cluster_tap_save [spc2]
+      expect {
+        described_class.new(existing: spc1, replacement: spc2).apply
+      }.to raise_error IndexError
+    end
+    it "when the :replacement hash cannot be turned into a commitment" do
+      cluster_tap_save [spc1]
+      expect {
+        described_class.new(existing: spc1, replacement: {}).apply
+      }.to raise_error ArgumentError
+    end
+  end
+
+  describe "#apply" do
+    it "replace an active commitment with an existing commitment" do
+      # Setup
+      cluster_tap_save [spc1, spc2]
+      # Pre-check
+      expect(SharedPrint::Finder.new.commitments.to_a.size).to eq 2
+      expect(spc1.deprecated?).to be false
+      # Action
+      rep_rec = described_class.new(existing: spc1, replacement: spc2)
+      rep_rec.apply
+      # Post-check
+
+      deprecated = get_deprecated
+      active = get_active
+      expect(deprecated.size).to eq 1
+      expect(active.size).to eq 1
+      expect(deprecated.first.local_id).to eq spc1.local_id
+      expect(rep_rec.verify).to be true
+    end
+    it "replace a deprecated commitment with an existing commitment" do
+      # Setup
+      cluster_tap_save [spc1, spc2]
+      spc1.deprecate(status: "E")
+      # Pre-check
+      expect(spc1.deprecated?).to be true
+      # Action
+      rep_rec = described_class.new(existing: spc1, replacement: spc2)
+      rep_rec.apply
+      # Post-check
+      deprecated = get_deprecated
+      active = get_active
+      expect(deprecated.size).to eq 1
+      expect(active.size).to eq 1
+      expect(deprecated.first.local_id).to eq spc1.local_id
+      expect(rep_rec.verify).to be true
+    end
+    it "replace an active commitment with a new commitment" do
+      # Setup
+      cluster_tap_save [spc1]
+      # Pre-check
+      expect(spc1.deprecated?).to be false
+      # Action
+      rep_rec = described_class.new(existing: spc1, replacement: spc2_hash)
+      rep_rec.apply
+      # Post-check
+      deprecated = get_deprecated
+      active = get_active
+      expect(deprecated.size).to eq 1
+      expect(active.size).to eq 1
+      expect(deprecated.first.local_id).to eq spc1.local_id
+      expect(rep_rec.verify).to be true
+    end
+    it "replace a deprecated commitment with a new commitment" do
+      # Setup
+      cluster_tap_save [spc1]
+      spc1.deprecate(status: "E")
+      # Pre-check
+      expect(spc1.deprecated?).to be true
+      # Action
+      rep_rec = described_class.new(existing: spc1, replacement: spc2_hash)
+      rep_rec.apply
+      # Post-check
+      deprecated = get_deprecated
+      active = get_active
+      expect(deprecated.size).to eq 1
+      expect(active.size).to eq 1
+      expect(deprecated.first.local_id).to eq spc1.local_id
+      expect(rep_rec.verify).to be true
+    end
+  end
+
+  describe "#apply_broken" do
+    it "copy of 'replace an active commitment with an existing commitment' using broken_apply, left in for science" do
+      # Setup
+      cluster_tap_save [spc1, spc2]
+      # Pre-check
+      expect(SharedPrint::Finder.new.commitments.to_a.size).to eq 2
+      expect(spc1.deprecated?).to be false
+      # Action
+      rep_rec = described_class.new(existing: spc1, replacement: spc2)
+      rep_rec.apply_broken
+      # Post-check
+      deprecated = get_deprecated
+      active = get_active
+      expect(deprecated.size).to eq 1
+      # This, active.size == 2, is proof an extra copy gets made,
+      # should be 1 like the other tests
+      expect(active.size).to eq 2
+      expect(deprecated.first.local_id).to eq spc1.local_id
+      expect(rep_rec.verify).to be true
+    end
+  end
+end
