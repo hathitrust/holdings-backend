b6fa66fc794baa1a396de107dd28034093e7eccf
Aaron Elkiss <aelkiss@umich.edu>
2022-05-12 17:12:23 -0400

    Use pushgateway in dev
    Some scripts in dev failed because the pushgateway was not started by
    default or referenced by development config. This starts and enables it.

--- bin/setup/setup_dev.sh
+++ bin/setup/setup_dev.sh
@@ -2,7 +2,7 @@
 
 $(dirname ${BASH_SOURCE[0]})/setup_test.sh
 
-docker-compose up -d mongo_dev
+docker-compose up -d mongo_dev pushgateway
 docker-compose run --rm dev bin/setup/wait-for mongo_dev:27017 -- echo "mongo is ready"
 docker-compose exec -T mongo_dev bash /tmp/bin/setup/rs_initiate.sh mongo_dev
 docker-compose run --rm -e MONGOID_ENV=development dev bundle exec ruby lib/tasks/build_database.rb
