463ac5321599cc50d44c67fc64fe8e19884c2425
Josh Steverman <jstever@umich.edu>
2022-06-15 13:27:32 -0400

    Increase length limit on holdings_htitem_htmember table

--- sql/003_holdings_htitem_htmember.sql
+++ sql/003_holdings_htitem_htmember.sql
@@ -1,10 +1,10 @@
 USE `ht_repository`;
 
 CREATE TABLE IF NOT EXISTS `holdings_htitem_htmember` (
-  `lock_id` varchar(100) NOT NULL,
+  `lock_id` varchar(300) NOT NULL,
   `cluster_id` varchar(25) NOT NULL,
   `volume_id` varchar(50) NOT NULL,
-  `n_enum` varchar(50) DEFAULT '',
+  `n_enum` varchar(250) DEFAULT '',
   `member_id` varchar(20) NOT NULL,
   `copy_count` int(11) DEFAULT NULL,
   `lm_count` smallint(6) DEFAULT NULL,
