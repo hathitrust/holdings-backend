0a29d12ffc2804b7966b771838ba0263642325e9
mw <mwarin@umich.edu>
2022-08-23 14:22:51 -0400

    init

---
+++ lib/scrub/scrub_runner.rb
@@ -0,0 +1,125 @@
+# A fully automated scrubber would:
+# Check member directories for new member-submitted files
+# Compare remote files to processed files
+# Copy new files to work directory
+# Validate and scrub files into loadable format
+# Load files into db
+# Upload report to member dir
+# ... should be able to do this for any number of members/files, at a whim or by cron,
+# ideally using a queue or pool of workers.
+
+require "data_sources/ht_organizations"
+require "data_sources/directory_locator"
+require "loader/file_loader"
+require "loader/holding_loader"
+require "scrub/autoscrub"
+require "utils/file_transfer"
+require "fileutils"
+
+# Example:
+# runner = Scrub::ScrubRunner.new
+# runner.run_all_members
+# runner.run_some_members(["umich", ..., "harvard"])
+# runner.run_one_member("umich")
+
+module Scrub
+  # Scrubs and loads any new member-uploaded files.
+  class ScrubRunner
+    def initialize
+      if Settings.local_member_dir.nil?
+        raise "Need Settings.local_member_dir to be set"
+      end
+      if Settings.remote_member_dir.nil?
+        raise "Need Settings.remote_member_dir to be set"
+      end
+
+      @ft = Utils::FileTransfer.new
+    end
+
+    def run_all_members
+      puts "Run all members."
+      @members = DataSources::HTOrganizations.new.members.keys.sort
+      run_some_members(@members)
+    end
+
+    def run_some_members(members)
+      puts "Run members: #{members}."
+      members.each do |member|
+        run_one_member(member)
+      end
+    end
+
+    def run_one_member(member)
+      puts "Running member #{member}."
+      new_files = check_new_files(member)
+      puts "Found #{new_files.size} new files: #{new_files.join(", ")}."
+      new_files.each do |new_file|
+        run_file(member, file)
+      end
+    end
+
+    # This should ideally spin off another (set of?) worker thread(s).
+    def run_file(member, file)
+      puts "running file #{file} for member #{member}"
+      download_to_work_dir(member, new_file)
+      scrubber = Scrub::Autoscrub.new(new_file)
+      scrubber.run
+      # todo: chunk and hand off job to sidekiq
+      # todo: figure out the output files that was just generated
+      upload_to_member(member, scrubber.reports.latest)
+      holding_loader = Loader::HoldingLoader.for(scrubber.output.latest)
+      Loader::FileLoader.new(holding_loader).load(scrubber.output.latest)
+    end
+
+    # Check member-uploaded files for any not previously seen files
+    def check_new_files(member)
+      puts "check new files for member #{member}"
+      remote_dir = DataSources::DirectoryLocator.new(
+        Settings.remote_member_dir,
+        member
+      ).holdings_current
+      remote_files = @ft.lsjson(remote_dir).map { |f| f["Name"] }
+      old_files = check_old_files(member)
+      # Return new (as in previously not processed) files
+      puts "MATH"
+      new_files = remote_files - old_files
+      puts "new(#{new_files.join(", ")}) = remote(#{remote_files.join(", ")}) - old(#{old_files.join(", ")})"
+      puts "found #{new_files.size} new files"
+      new_files
+    end
+
+    # Check the member scrub_dir for previously scrubbed files.
+    def check_old_files(member)
+      puts "check old files for #{member}"
+      local_dir = DataSources::DirectoryLocator.new(
+        Settings.local_member_dir,
+        member
+      ).holdings_current
+      @ft.lsjson(local_dir).map { |f| f["Name"] }      
+    end
+
+    def download_to_work_dir(member, file)
+      puts "download remote file #{file} to work dir for #{member}"
+
+      work_dir = DataSources::DirectoryLocator.new(
+        Settings.local_member_dir,
+        member
+      ).base + "/work"
+
+      @ft.download(file, work_dir)
+
+      work_dir + "/#{file}"
+    end
+
+    def upload_to_member(member, file)
+      puts "upload local file #{file} to remote dir for #{member}"
+      # todo: something something rclone, upload scrub report to dropbox
+    end
+
+    def move_to_scrubbed_dir(member, file)
+      scrubbed_dir = "#{Settings.local_member_dir}/#{member}"
+      puts "move #{file} to #{scrubbed_dir}"
+      FileUtils.mv(file, scrubbed_dir)
+    end
+  end
+end
---
+++ spec/scrub/scrub_runner_spec.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "scrub/scrub_runner"
+require "data_sources/directory_locator"
+
+RSpec.describe Scrub::ScrubRunner do
+  Settings.rclone_config_path = "/tmp/rclone.conf"
+  Settings.local_member_dir = "/tmp/local_member_dir"
+  Settings.remote_member_dir = "/tmp/remote_member_dir"
+
+  let(:org1) { "test" }
+  
+  before(:each) do
+    FileUtils.touch(Settings.rclone_config_path)
+    FileUtils.mkdir_p(Settings.local_member_dir)
+    FileUtils.mkdir_p(Settings.remote_member_dir)
+  end
+
+  after(:each) do
+    FileUtils.rm_f(Settings.rclone_config_path)
+    FileUtils.rm_rf(Settings.local_member_dir)
+    FileUtils.rm_rf(Settings.remote_member_dir)
+  end
+
+  describe "#check_old_files" do
+    it "local_d needs to exist first" do
+      local_d = DataSources::DirectoryLocator.new(Settings.local_member_dir, org1)
+      expect { described_class.new.check_old_files(org1) }.to raise_error Utils::FileTransferError
+      local_d.ensure!
+      expect { described_class.new.check_old_files(org1) }.to_not raise_error
+    end
+
+    it "gets the files in the requested dir" do
+      local_d = DataSources::DirectoryLocator.new(Settings.local_member_dir, org1)
+      local_d.ensure!
+      FileUtils.touch(File.join(local_d.holdings_current, "a.txt"))
+      FileUtils.touch(File.join(local_d.holdings_current, "b.txt"))
+      expect(described_class.new.check_old_files(org1)).to eq ["a.txt", "b.txt"]
+    end
+  end
+  
+  describe "#check_new_files" do
+    it "needs both remote and local dirs to exist" do
+      local_d = DataSources::DirectoryLocator.new(Settings.local_member_dir, org1)
+      remote_d = DataSources::DirectoryLocator.new(Settings.remote_member_dir, org1)
+      # Neither exist, raise
+      expect { described_class.new.check_new_files(org1) }.to raise_error Utils::FileTransferError
+      # One exists, the other does not, raise
+      local_d.ensure!
+      expect { described_class.new.check_new_files(org1) }.to raise_error Utils::FileTransferError
+      # Both exist, OK.
+      remote_d.ensure!
+      expect { described_class.new.check_new_files(org1) }.to_not raise_error
+    end
+
+    it "lists files in the remote dir whose names do not match old files in the local dir" do
+      local_d = DataSources::DirectoryLocator.new(Settings.local_member_dir, org1)
+      remote_d = DataSources::DirectoryLocator.new(Settings.remote_member_dir, org1)
+      local_d.ensure!
+      remote_d.ensure!
+      # When there are no files:
+      expect(described_class.new.check_new_files(org1)).to eq []
+
+      # When there are remote files but no local files:
+      FileUtils.touch(File.join(remote_d.holdings_current, "a.tsv"))
+      expect(described_class.new.check_new_files(org1)).to eq ["a.tsv"]
+
+      # When remote files == local files
+      FileUtils.touch(File.join(local_d.holdings_current, "a.tsv"))
+      expect(described_class.new.check_new_files(org1)).to eq []
+
+      # When there is a remote file not in local
+      FileUtils.touch(File.join(remote_d.holdings_current, "b.tsv"))
+      expect(described_class.new.check_new_files(org1)).to eq ["b.tsv"]
+    end
+  end
+  
+  # test everything
+  it "#run_some_members" do
+    orgs = ["smu", "umich"]
+    roots = [Settings.local_member_dir, Settings.remote_member_dir]
+    # Make sure the dirs exist first
+    roots.each do |root|
+      orgs.each do |org|
+        DataSources::DirectoryLocator.new(root, org).ensure!
+      end
+    end
+    expect { described_class.new.run_some_members(orgs) }.not_to raise_error
+  end
+end
