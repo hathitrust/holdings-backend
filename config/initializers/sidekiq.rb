4bdc5ceffd32290501dd4f10183525c29d2d7762
Aaron Elkiss <aelkiss@umich.edu>
2022-10-17 13:56:40 -0400

    Conditionally use mongo sentinel

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,24 @@
 require "sidekiq"
+require "services"
 
 # Add anything here you need for sidekiq initialization
 
-redis_config = {
-  url: "redis://redis"
-}
+if ENV["REDIS_MASTER_SET_NAME"] && ENV["REDIS_HEADLESS_SERVICE"]
+  require "resolv"
+
+  # https://github.com/mperham/sidekiq/issues/5194
+  redis_config = {
+    host: ENV["REDIS_MASTER_SET_NAME"],
+    password: ENV["REDIS_PASSWORD"],
+    sentinels: Resolv.getaddresses(ENV["REDIS_HEADLESS_SERVICE"]).map do |address|
+      {host: address, port: 26379, password: ENV["REDIS_PASSWORD"]}
+    end
+  }
+else
+  redis_config = {
+    url: Settings.redis_url || "redis://redis"
+  }
+end
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
