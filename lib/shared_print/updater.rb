1a77d021cf6ccd08c7ce650b6f0257c4c752d131
mw <mwarin@umich.edu>
2022-11-11 09:56:29 -0500

    move commitment to another cluster if updating commitment OCN

--- lib/shared_print/updater.rb
+++ lib/shared_print/updater.rb
@@ -51,12 +51,47 @@ module SharedPrint
     # Apply a set of changes from 1 SharedPrint::UpdateRecord to one Clusterable::Commitment.
     def apply_single_update
       commitment = @commitments.first
+      please_update_ocn = false
+      new_ocn = nil
       @update_record.updater_fields.each do |k, v|
+        if k == :new_ocn
+          # All other updates are easy, but ocn is tricky so we do that in a different pass
+          please_update_ocn = true
+          new_ocn = v
+          next
+        end
         report "updating #{k}=#{v}"
         commitment.send("#{strip_new_from_symbol(k)}=", v)
       end
       report "OK."
       commitment.save
+      update_ocn(commitment, new_ocn) if please_update_ocn
+    end
+
+    def update_ocn(commitment, new_ocn)
+      original_cluster = commitment.cluster
+      if original_cluster.ocns.include?(new_ocn)
+        report "updated ocn but staying on the same cluster"
+        commitment.ocn = new_ocn
+        commitment.save
+      else
+        # If we are updating ocn to another cluster, special treatment.
+        report "potentially moving clusters..."
+        move_to_cluster = Cluster.find_by(ocns: new_ocn)
+        # Check if the cluster we move to has ht_items and a matching holdings rec
+        if move_to_cluster.nil?
+          raise "no cluster for ocn #{new_ocn}, cannot move commitment"
+        elsif move_to_cluster.ht_items.empty?
+          raise "no htitems on cluster for ocn #{new_ocn}, cannot move commitment"
+        end
+        report "moved commitment FROM cluster <OCN:#{original_cluster.ocns.join(",")}>"
+        report "TO cluster <OCN:#{move_to_cluster.ocns.join(",")}>"
+        newc = commitment.clone
+        newc.ocn = new_ocn
+        commitment.delete
+        # Add commitment to new cluster and save
+        move_to_cluster.add_commitments([newc])
+      end
     end
 
     # Remove /^new_/ from :new_local_id and :new_ocn.
