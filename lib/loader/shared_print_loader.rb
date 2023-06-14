96917948923727d577ce09de6f58998e20888203
mw <mwarin@umich.edu>
2023-06-14 15:02:15 -0400

    added support for other_commitments

--- lib/loader/shared_print_loader.rb
+++ lib/loader/shared_print_loader.rb
@@ -38,11 +38,36 @@ module Loader
   #
   ## Subclass that only overrides item_from_line
   class SharedPrintLoaderTSV < SharedPrintLoader
+    @@colon_sep_vals = /.+:.+/
     def item_from_line(line)
       # Utils::TSVReader takes care of parsing into fields, but we still need
       # to clean them up
       fields = line.compact.reject { |k, v| v.empty? }
-      fields[:policies] = fields.fetch(:policies, "").downcase.split(",")
+      # Some fields need specific case. Todo: harmonize this.
+      downcase_fields = [:local_shelving_type, :policies]
+      downcase_fields.each do |sym|
+        fields[sym] = fields[sym]&.downcase
+      end
+      upcase_fields = [:retention_condition]
+      upcase_fields.each do |sym|
+        fields[sym] = fields[sym]&.upcase
+      end
+
+      # policies is an array and comes in as a comma-sep string
+      unless fields[:policies].nil?
+        fields[:policies] = fields[:policies].split(",")
+      end
+
+      # other_commitments is an input that is stored as
+      # other_program and other_retention_date
+      if fields.key?(:other_commitments)
+        if @@colon_sep_vals.match?(fields[:other_commitments])
+          prog, date = fields[:other_commitments].split(":")
+          fields[:other_program] = prog
+          fields[:other_retention_date] = DateTime.parse(date)
+        end
+        fields.delete(:other_commitments)
+      end
 
       Clusterable::Commitment.new(fields)
     end
