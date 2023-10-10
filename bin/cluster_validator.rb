16eebf741930b13e254ec298b3f7778ae434aa63
mw <mwarin@umich.edu>
2023-10-10 18:15:34 -0400

    Adding a cluster validator script

---
+++ bin/cluster_validator.rb
@@ -0,0 +1,35 @@
+require "cluster"
+require "services"
+Services.mongo!
+
+# Goes through all clusters, checks if they are valid,
+# and prints the first ocn of any invalid cluster to a file.
+
+class ClusterValidator
+  attr_reader :output_path # file name
+  def initialize
+    # Make an output file in the right place
+    ymd = Time.now.strftime("%Y-%m-%d")
+    dir = Settings.local_report_path
+    FileUtils.mkdir_p(dir)
+    @output_path = "#{dir}/cluster_validator_#{ymd}.txt"
+  end
+
+  def run
+    puts "Writing to #{output_path}"
+    File.open(output_path, "w") do |outf|
+      # Go through each cluster and check if valid.
+      outf.puts "# These are ocns of invalid clusters:"
+      Cluster.each do |c|
+        unless c.valid?
+          outf.puts(c.ocns.first)
+        end
+      end
+      outf.puts "# Done"
+    end
+  end
+end
+
+if __FILE__ == $0
+  ClusterValidator.new.run
+end
