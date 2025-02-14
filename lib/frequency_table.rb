b39d770347ade01cfad847a4849a25c11cc824ab
Brian Moses Hall <moseshll@umich.edu>
2025-02-14 11:08:03 -0500

    DEV-1533 FrequencyTable: implement `+` method - We base `+` on a new public method `append!` which does the heavy lifting. - We discussed a number of possible optimizations, including copying parts of the addend's data structure   - Unfortunately this approach runs the risk of clone/dup copying by reference and resulting in two FTs sharing data.   - Added an `each` method so large tables need not be cloned with `to_h` - If it turns out we don't need to be so rigorous about protecting the internals of this class, we can just make an accessor to `@table`   - Our implementation of `to_h` (`.clone.freeze`) is flawed in that the return is not a deep copy anyway.

--- lib/frequency_table.rb
+++ lib/frequency_table.rb
@@ -25,6 +25,49 @@ class FrequencyTable
     @table.clone.freeze
   end
 
+  # Lower-memory alternative to `to_h` for the purposes of `+` and `append!`
+  def each
+    @table.each do |key, value|
+      yield key, value
+    end
+  end
+
+  # Create a public #append(ft) method that we can call on a clone?
+  def +(other)
+    new_obj = self.class.new
+    new_obj.append! self
+    new_obj.append! other
+  end
+
+  # We try to optimize this beyond a deep addition from the leaves inward by
+  # checking for missing keys and when possible copying chunks of the operand's
+  # data structure.
+  def append!(other)
+    other.each do |org, data|
+      # Example data: org = :umich, data = {:spm=>{1=>1}}
+      #
+      # One could try to optimize this level by detecting missing `org` keys
+      # in the receiver and copying over an entire chunk of data structure from `other`
+      # but subsequent changes to `self` can propagate to `other`.
+      # `Marshal` can't handle the funky initializer on `@data`
+      # ("can't dump hash with default proc") so that deep copy hack doesn't seem
+      # available to us.
+      data.each do |fmt, frequencies|
+        # Example data: fmt = :spm, frequencies = {1 => 1}
+        #
+        # Safe to shallow clone `frequencies` since its keys and values are scalars.
+        if !@table[org].key? fmt
+          @table[org][fmt] = frequencies.clone
+          next
+        end
+        frequencies.each do |bucket, count|
+          @table[org][fmt][bucket] += count
+        end
+      end
+    end
+    self
+  end
+
   def add_ht_item(ht_item)
     item_format = CalculateFormat.new(ht_item.cluster).item_format(ht_item).to_sym
     item_overlap = Overlap::HtItemOverlap.new(ht_item)
