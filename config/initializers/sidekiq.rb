e86b72a692bededa55c2c961d03c4b459f411c9a
Aaron Elkiss <aelkiss@umich.edu>
2025-02-18 09:37:07 -0500

    Try again with sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,7 +6,7 @@ require "services"
 if ENV["REDIS_SIDEKIQ_RW_HOST"] && ENV["REDIS_SIDEKIQ_RW_PASSWORD"]
   Services.register(:redis_config) do
     {
-      name: ENV["REDIS_SIDEKIQ_RW_HOST"],
+      host: ENV["REDIS_SIDEKIQ_RW_HOST"],
       password: ENV["REDIS_SIDEKIQ_RW_PASSWORD"],
     }
   end
